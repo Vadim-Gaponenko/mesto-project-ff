12049dd6f3efdfb502b6eee0787ef86de91af311
Vadim-Gaponenko <139169525+Vadim-Gaponenko@users.noreply.github.com>
2023-11-02 15:03:14 +0300

    new file

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template");
 
 // @todo: DOM узлы
-
+const placesList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function creatingCards(iteam) {
+  const cardTemplateContent = cardTemplate.content;
+  const element = cardTemplateContent.cloneNode(true);
+  const imgCard = element.querySelector(".card__image");
+  const titleCard = element.querySelector(".card__title");
+  const buttonLike = element.querySelector(".card__like-button");
+  imgCard.src = iteam.link;
+  titleCard.name = iteam.name;
+  return element;
+}
+function addCards() {
+  initialCards.forEach((iteam) => {
+    const cardItem = creatingCards(iteam);
+    const deleteButton = cardItem.querySelector(".card__delete-button");
+    deleteButton.addEventListener("click", deleteCard);
+    placesList.append(cardItem);
+  });
+}
+function deleteCard(event) {
+  const card = event.target.closest(".places__item");
+  card.remove();
+}
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+addCards();
